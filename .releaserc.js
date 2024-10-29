9a23a2c55e8b7a53029620d1742e38a912eb02fa
lonerapier <lonerapier@proton.me>
2024-10-29 17:05:36 +0530

    do not publish to npm on each release

--- .releaserc.js
+++ .releaserc.js
@@ -4,7 +4,12 @@ module.exports = {
     plugins: [
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
-        '@semantic-release/npm',
+        [
+            "@semantic-release/npm",
+            {
+                "npmPublish": false
+            }
+        ],
         '@semantic-release/github',
         [
             '@semantic-release/git',
