30d86f80b2c7d441aada8cf1d17e6f3e5c1c7067
lonerapier <lonerapier@proton.me>
2024-11-05 12:28:15 +0530

    complete full test

--- circuits/test/full/full.test.ts
+++ circuits/test/full/full.test.ts
@@ -42,6 +42,10 @@ describe("NIVC_FULL", async () => {
     let httpParseAndLockStartLineCircuit: WitnessTester<["step_in", "beginning", "beginning_length", "middle", "middle_length", "final", "final_length"], ["step_out"]>;
     let lockHeaderCircuit: WitnessTester<["step_in", "header", "headerNameLength", "value", "headerValueLength"], ["step_out"]>;
     let bodyMaskCircuit: WitnessTester<["step_in"], ["step_out"]>;
+    let parse_circuit: WitnessTester<["step_in"], ["step_out"]>;
+    let json_mask_object_circuit: WitnessTester<["step_in", "key", "keyLen"], ["step_out"]>;
+    let json_mask_arr_circuit: WitnessTester<["step_in", "index"], ["step_out"]>;
+    let extract_value_circuit: WitnessTester<["step_in"], ["step_out"]>;
 
     const DATA_BYTES = 320;
     const MAX_STACK_HEIGHT = 5;
@@ -57,6 +61,9 @@ describe("NIVC_FULL", async () => {
     const beginning = [72, 84, 84, 80, 47, 49, 46, 49]; // HTTP/1.1
     const middle = [50, 48, 48]; // 200
     const final = [79, 75]; // OK
+
+    const MAX_KEY_LENGTH = 8;
+    const MAX_VALUE_LENGTH = 35;
 
     before(async () => {
         aesCircuit = await circomkit.WitnessTester("AESGCTRFOLD", {
@@ -85,6 +92,34 @@ describe("NIVC_FULL", async () => {
             params: [DATA_BYTES, MAX_STACK_HEIGHT],
         });
         console.log("#constraints (HTTP-BODY-MASK):", await bodyMaskCircuit.getConstraintCount());
+
+        parse_circuit = await circomkit.WitnessTester(`JsonParseNIVC`, {
+            file: "json/nivc/parse",
+            template: "JsonParseNIVC",
+            params: [DATA_BYTES, MAX_STACK_HEIGHT],
+        });
+        console.log("#constraints (JSON-PARSE):", await parse_circuit.getConstraintCount());
+
+        json_mask_arr_circuit = await circomkit.WitnessTester(`JsonMaskArrayIndexNIVC`, {
+            file: "json/nivc/masker",
+            template: "JsonMaskArrayIndexNIVC",
+            params: [DATA_BYTES, MAX_STACK_HEIGHT],
+        });
+        console.log("#constraints (JSON-MASK-ARRAY-INDEX):", await json_mask_arr_circuit.getConstraintCount());
+
+        json_mask_object_circuit = await circomkit.WitnessTester(`JsonMaskObjectNIVC`, {
+            file: "json/nivc/masker",
+            template: "JsonMaskObjectNIVC",
+            params: [DATA_BYTES, MAX_STACK_HEIGHT, MAX_KEY_LENGTH],
+        });
+        console.log("#constraints (JSON-MASK-OBJECT):", await json_mask_object_circuit.getConstraintCount());
+
+        extract_value_circuit = await circomkit.WitnessTester(`JsonMaskExtractFinal`, {
+            file: "json/nivc/extractor",
+            template: "MaskExtractFinal",
+            params: [DATA_BYTES, MAX_STACK_HEIGHT, MAX_VALUE_LENGTH],
+        });
+        console.log("#constraints (JSON-MASK-EXTRACT-FINAL):", await extract_value_circuit.getConstraintCount());
     });
 
 
@@ -127,6 +162,33 @@ describe("NIVC_FULL", async () => {
         let maskedInput = extendedJsonInput.fill(0, 0, idx);
         maskedInput = maskedInput.fill(0, 320);
 
-        bodyMaskOut === maskedInput;
+
+
+        let key0 = [100, 97, 116, 97, 0, 0, 0, 0]; // "data"
+        let key0Len = 4;
+        let key1 = [105, 116, 101, 109, 115, 0, 0, 0]; // "items"
+        let key1Len = 5;
+        let key2 = [112, 114, 111, 102, 105, 108, 101, 0]; // "profile"
+        let key2Len = 7;
+        let key3 = [110, 97, 109, 101, 0, 0, 0, 0]; // "name"
+        let key3Len = 4;
+
+        let value = toByte("\"Taylor Swift\"");
+
+        let json_parse = await parse_circuit.compute({ step_in: bodyMaskOut }, ["step_out"]);
+
+        let json_extract_key0 = await json_mask_object_circuit.compute({ step_in: json_parse.step_out, key: key0, keyLen: key0Len }, ["step_out"]);
+
+        let json_num = json_extract_key0.step_out as number[];
+        console.log("json_extract_key0", json_num);
+        let json_extract_key1 = await json_mask_object_circuit.compute({ step_in: json_extract_key0.step_out, key: key1, keyLen: key1Len }, ["step_out"]);
+
+        let json_extract_arr = await json_mask_arr_circuit.compute({ step_in: json_extract_key1.step_out, index: 0 }, ["step_out"]);
+
+        let json_extract_key2 = await json_mask_object_circuit.compute({ step_in: json_extract_arr.step_out, key: key2, keyLen: key2Len }, ["step_out"]);
+
+        let json_extract_key3 = await json_mask_object_circuit.compute({ step_in: json_extract_key2.step_out, key: key3, keyLen: key3Len }, ["step_out"]);
+
+        await extract_value_circuit.expectPass({ step_in: json_extract_key3.step_out }, { step_out: value });
     });
 });
