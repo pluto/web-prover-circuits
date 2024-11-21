9c11e8d19513a90bfef1cd3f25c0ef6699b6186c
Colin Roberts <colin@autoparallel.xyz>
2024-11-21 15:40:15 -0700

    wip: prep to serialize pk/vk

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
 use anyhow::{Context, Result};
+use client_side_prover::{
+    provider::{hyperkzg::EvaluationEngine, Bn256EngineKZG, GrumpkinEngine},
+    spartan::batched::BatchedRelaxedR1CSSNARK,
+    supernova::snark::CompressedSNARK,
+};
 use proofs::program::{
     self,
     data::{ProgramData, SetupData},
@@ -7,6 +12,13 @@ use proofs::program::{
 use std::fs::{self, File};
 use std::io::Write;
 use std::path::{Path, PathBuf};
+
+pub type E1 = Bn256EngineKZG;
+pub type E2 = GrumpkinEngine;
+pub type EE1 = EvaluationEngine<halo2curves::bn256::Bn256, E1>;
+pub type EE2 = client_side_prover::provider::ipa_pc::EvaluationEngine<E2>;
+pub type S1 = BatchedRelaxedR1CSSNARK<E1, EE1>;
+pub type S2 = BatchedRelaxedR1CSSNARK<E2, EE2>;
 
 struct CircuitFiles {
     r1cs_path: PathBuf,
@@ -92,6 +104,37 @@ fn main() -> Result<()> {
 
     println!("Generating public parameters...");
     let public_params = program::setup(&setup_data);
+
+    // TODO (autoparallel): Implement serde on the `ProverKey` and `VerifierKey` then uncomment this
+    // let (pk, vk) = CompressedSNARK::<E1, S1, S2>::setup(&public_params)?;
+
+    // Write out the `ProverKey`
+    // let serialized_pk =
+    //     bincode::serialize(&pk).context("Failed to serialize auxiliary parameters")?;
+
+    // let output_file = artifacts_dir.join(format!("prover_key_{}.bin", target_size));
+    // println!("Writing output to: {}", output_file.display());
+
+    // let mut file = File::create(&output_file)
+    //     .with_context(|| format!("Failed to create output file: {}", output_file.display()))?;
+
+    // file.write_all(&serialized_pk)
+    //     .with_context(|| format!("Failed to write to output file: {}", output_file.display()))?;
+
+    // // Write out the `VerifierKey`
+    // let serialized_vk =
+    //     bincode::serialize(&vk).context("Failed to serialize auxiliary parameters")?;
+
+    // let output_file = artifacts_dir.join(format!("prover_key_{}.bin", target_size));
+    // println!("Writing output to: {}", output_file.display());
+
+    // let mut file = File::create(&output_file)
+    //     .with_context(|| format!("Failed to create output file: {}", output_file.display()))?;
+
+    // file.write_all(&serialized_vk)
+    //     .with_context(|| format!("Failed to write to output file: {}", output_file.display()))?;
+
+    // Write out the `AuxParams`
     let aux_params = public_params.aux_params();
     let serialized_aux_params =
         bincode::serialize(&aux_params).context("Failed to serialize auxiliary parameters")?;
