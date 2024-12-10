19f05799833d29cf6245b344c2b79cf66e66ccc8
Colin Roberts <colin@autoparallel.xyz>
2024-12-10 13:27:16 -0700

    rename

--- circuits/test/json/parser/hash_parser.test.ts
+++ circuits/test/json/parser/hash_parser.test.ts
@@ -1,43 +1,23 @@
-import { circomkit, WitnessTester, generateDescription, readJSONInputFile } from "../../common";
-import { PoseidonModular } from "../../common/poseidon";
+import { circomkit, WitnessTester, readJSONInputFile } from "../../common";
 
-describe("hash_machine", () => {
-    let circuit: WitnessTester<["data"]>;
+describe("Hash Parser", () => {
+    let hash_parser: WitnessTester<["data"]>;
 
     it(`input: array_only`, async () => {
         let filename = "array_only";
-        let [input, keyUnicode, output] = readJSONInputFile(`${filename}.json`, []);
+        let [input, _keyUnicode, _output] = readJSONInputFile(`${filename}.json`, []);
 
-        circuit = await circomkit.WitnessTester(`Parser`, {
+        hash_parser = await circomkit.WitnessTester(`Parser`, {
             file: "json/parser/hash_parser",
             template: "ParserHasher",
             params: [input.length, 3],
         });
-        console.log("#constraints:", await circuit.getConstraintCount());
+        console.log("#constraints:", await hash_parser.getConstraintCount());
 
-        await circuit.expectPass({
+        await hash_parser.expectPass({
             data: input
         });
     });
-
-    // Numbers for the 42 read in 0th index
-    console.log("[0,\"4\"] hash: ", PoseidonModular([0, 52]));
-    console.log("[prev,\"2\"] hash: ", PoseidonModular([BigInt("10851631763548351427431043290272583122934382613350600043660274710013149244741"), 50]));
-
-    // Number for the "a"
-    console.log("[0,\"a\"] hash: ", PoseidonModular([0, 97]));
-    // Numbers for the "b" read inside object in 1st index
-    console.log("[0,\"b\"] hash: ", PoseidonModular([0, 98]));
-
-    // console.log("[2,0] hash: ", PoseidonModular([2, 0]));
-    // console.log("[2,1] hash: ", PoseidonModular([2, 1]));
-    // console.log("[1,0] hash: ", PoseidonModular([1, 0]));
-    // [0,0] hash:  14744269619966411208579211824598458697587494354926760081771325075741142829156n
-    // [2,0] hash:  17525667638260400994329361135304146970274213890416440938331684485841550124768n
-    // [2,1] hash:  9708419728795563670286566418307042748092204899363634976546883453490873071450n
-    // [1,0] hash:  18423194802802147121294641945063302532319431080857859605204660473644265519999n
-
-    // TODO: Check that the hash of the packedState.in getting the next_state_hash is correct, the stack hashes are correct.
 
     // it(`input: value_array.json`, async () => {
     //     let filename = "value_array";
