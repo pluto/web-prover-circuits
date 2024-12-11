35943a25ed97755fc75c88bbd1be32bdfe080a32
Colin Roberts <colin@autoparallel.xyz>
2024-12-10 18:01:40 -0700

    it may be working!

--- circuits/test/json/parser/hash_parser.test.ts
+++ circuits/test/json/parser/hash_parser.test.ts
@@ -3,23 +3,23 @@ import { circomkit, WitnessTester, readJSONInputFile } from "../../common";
 describe("Hash Parser", () => {
     let hash_parser: WitnessTester<["data", "polynomial_input", "sequence_digest"]>;
 
-    it(`input: array_only`, async () => {
-        let filename = "array_only";
-        let [input, _keyUnicode, _output] = readJSONInputFile(`${filename}.json`, []);
+    // it(`input: array_only`, async () => {
+    //     let filename = "array_only";
+    //     let [input, _keyUnicode, _output] = readJSONInputFile(`${filename}.json`, []);
 
-        hash_parser = await circomkit.WitnessTester(`Parser`, {
-            file: "json/parser/hash_parser",
-            template: "ParserHasher",
-            params: [input.length, 3],
-        });
-        console.log("#constraints:", await hash_parser.getConstraintCount());
+    //     hash_parser = await circomkit.WitnessTester(`Parser`, {
+    //         file: "json/parser/hash_parser",
+    //         template: "ParserHasher",
+    //         params: [input.length, 3],
+    //     });
+    //     console.log("#constraints:", await hash_parser.getConstraintCount());
 
-        await hash_parser.expectPass({
-            data: input,
-            polynomial_input: 2,
-            sequence_digest: 1, // TODO: This isn't useful
-        });
-    });
+    //     await hash_parser.expectPass({
+    //         data: input,
+    //         polynomial_input: 2,
+    //         sequence_digest: 1, // TODO: This isn't useful
+    //     });
+    // });
 
     // it(`input: value_array.json`, async () => {
     //     let filename = "value_array";
@@ -55,20 +55,20 @@ describe("Hash Parser", () => {
     // });
 
 
-    // it(`spotify_input`, async () => {
-    //     let filename = "spotify";
-    //     let [input, keyUnicode, output] = readJSONInputFile(`${filename}.json`, []);
-    //     hash_parser = await circomkit.WitnessTester(`Parser`, {
-    //         file: "json/parser/hash_parser",
-    //         template: "ParserHasher",
-    //         params: [input.length, 5],
-    //     });
-    //     console.log("#constraints:", await hash_parser.getConstraintCount());
+    it(`spotify_input`, async () => {
+        let filename = "spotify";
+        let [input, keyUnicode, output] = readJSONInputFile(`${filename}.json`, []);
+        hash_parser = await circomkit.WitnessTester(`Parser`, {
+            file: "json/parser/hash_parser",
+            template: "ParserHasher",
+            params: [input.length, 5],
+        });
+        console.log("#constraints:", await hash_parser.getConstraintCount());
 
-    //     await hash_parser.expectPass({
-    //         data: input,
-    //         polynomial_input: 1,
-    //         sequence_digest: 234435029355,
-    //     });
-    // });
+        await hash_parser.expectPass({
+            data: input,
+            polynomial_input: 2,
+            sequence_digest: 234435029355,
+        });
+    });
 })
