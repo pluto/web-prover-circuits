f9c9448e9169a0cf1bddc710ec586418b809533a
lonerapier <lonerapier@proton.me>
2025-01-16 17:21:57 +0530

    add plaintext_index in authentication and support chaining

--- circuits/test/full/full.test.ts
+++ circuits/test/full/full.test.ts
@@ -31,7 +31,7 @@ const MAX_STACK_HEIGHT = 10;
 // These `check_*` are currently from Rust to ensure we have parity
 const check_ciphertext_digest = BigInt("5947802862726868637928743536818722886587721698845887498686185738472802646104");
 const check_ciphertext_digest_dup = BigInt(
-    "10533684210547111058940459285576829498255433421882238742557401503782754130789"
+    "9719560477146706366275627147615553588605800736547946825367443581777188004918"
 );
 const check_init_nivc_input = BigInt("10288873638660630335427615297930270928433661836597941144520949467184902553219");
 
@@ -51,7 +51,7 @@ function to_nonce(iv: Uint8Array, seq: number): Uint8Array {
 }
 
 describe("Example NIVC Proof", async () => {
-    let PlaintextAuthentication: WitnessTester<["step_in", "plaintext", "key", "nonce", "counter"], ["step_out"]>;
+    let PlaintextAuthentication: WitnessTester<["step_in", "plaintext", "key", "nonce", "counter", "plaintext_index_counter", "ciphertext_digest"], ["step_out"]>;
     let HTTPVerification: WitnessTester<["step_in", "ciphertext_digest", "data", "main_digests"], ["step_out"]>;
     let JSONExtraction: WitnessTester<["step_in", "ciphertext_digest", "data", "sequence_digest", "value_digest"], ["step_out"]>;
 
@@ -96,17 +96,18 @@ describe("Example NIVC Proof", async () => {
             key: keyIn,
             nonce: nonceIn,
             counter: counterBits,
+            plaintext_index_counter: 0,
+            ciphertext_digest,
         }, ["step_out"]);
 
-        const http_response_plaintext_digest = PolynomialDigest(http_response_0_padded, ciphertext_digest);
-        const http_response_plaintext_digest_hashed = poseidon1([http_response_plaintext_digest]);
-        const correct_plaintext_authentication_step_out = modAdd(init_nivc_input - ciphertext_digest, http_response_plaintext_digest_hashed);
+        const http_response_plaintext_digest = PolynomialDigest(http_response_0_padded, ciphertext_digest, BigInt(0));
+        const correct_plaintext_authentication_step_out = modAdd(init_nivc_input - ciphertext_digest, http_response_plaintext_digest);
         assert.deepEqual(plaintext_authentication.step_out, correct_plaintext_authentication_step_out);
 
         // Run HTTPVerification
-        const start_line_digest = PolynomialDigest(http_start_line, ciphertext_digest);
-        const header_0_digest = PolynomialDigest(http_header_0, ciphertext_digest);
-        const header_1_digest = PolynomialDigest(http_header_1, ciphertext_digest);
+        const start_line_digest = PolynomialDigest(http_start_line, ciphertext_digest, BigInt(0));
+        const header_0_digest = PolynomialDigest(http_header_0, ciphertext_digest, BigInt(0));
+        const header_1_digest = PolynomialDigest(http_header_1, ciphertext_digest, BigInt(0));
 
         let main_digests = Array(MAX_NUMBER_OF_HEADERS + 1).fill(0);
         main_digests[0] = start_line_digest;
@@ -123,13 +124,13 @@ describe("Example NIVC Proof", async () => {
 
         const padded_http_body = http_body.concat(Array(DATA_BYTES - http_body.length).fill(0));
         let http_verification_step_out = BigInt((http_verification.step_out as number[])[0]);
-        let body_digest = PolynomialDigest(http_body, ciphertext_digest);
+        let body_digest = PolynomialDigest(http_body, ciphertext_digest, BigInt(0));
 
         const body_digest_hashed = poseidon1([body_digest]);
         const start_line_digest_digest_hashed = poseidon1([start_line_digest]);
         const header_0_digest_hashed = poseidon1([header_0_digest]);
         const header_1_digest_hashed = poseidon1([header_1_digest]);
-        const correct_http_verification_step_out = modAdd(step_in - start_line_digest_digest_hashed - header_0_digest_hashed - header_1_digest_hashed - http_response_plaintext_digest_hashed, body_digest_hashed);
+        const correct_http_verification_step_out = modAdd(step_in - start_line_digest_digest_hashed - header_0_digest_hashed - header_1_digest_hashed - http_response_plaintext_digest, body_digest_hashed);
         assert.deepEqual(http_verification_step_out, correct_http_verification_step_out);
 
         // Run JSONExtraction
@@ -148,7 +149,7 @@ describe("Example NIVC Proof", async () => {
 
         const [stack, treeHashes] = jsonTreeHasher(ciphertext_digest, keySequence, MAX_STACK_HEIGHT);
         const sequence_digest = compressTreeHash(ciphertext_digest, [stack, treeHashes]);
-        const value_digest = PolynomialDigest(targetValue, ciphertext_digest);
+        const value_digest = PolynomialDigest(targetValue, ciphertext_digest, BigInt(0));
 
         let json_extraction = await JSONExtraction.compute({
             step_in: http_verification_step_out,
@@ -163,15 +164,29 @@ describe("Example NIVC Proof", async () => {
     it("multiple ciphertext packets", async () => {
         // Run PlaintextAuthentication
 
+        assert.deepEqual(http_response_ciphertext.length / 2, http_response_ciphertext_dup.length);
+
+        let http_response_plaintext_1 = http_response_plaintext.slice(0, http_response_plaintext.length / 2);
+        let http_response_plaintext_2 = http_response_plaintext.slice(http_response_plaintext.length / 2);
+        let http_response_ciphertext_1 = http_response_ciphertext.slice(0, http_response_ciphertext.length / 2);
+        let http_response_ciphertext_2 = http_response_ciphertext_dup;
+
         let http_response_padded = http_response_plaintext.concat(Array(DATA_BYTES - http_response_plaintext.length).fill(-1));
-        let http_response_0_padded = http_response_plaintext.concat(Array(DATA_BYTES - http_start_line.length).fill(0));
-        let ciphertext_padded = http_response_ciphertext.concat(Array(DATA_BYTES - http_response_ciphertext.length).fill(-1));
-        let ciphertext_padded_2 = http_response_ciphertext_dup.concat(Array(DATA_BYTES - http_response_ciphertext_dup.length).fill(-1));
-        assert.deepEqual(http_response_ciphertext.length, http_response_ciphertext_dup.length);
+        let http_response1_padded = http_response_plaintext_1.concat(Array(DATA_BYTES - http_response_plaintext_1.length).fill(-1));
+        let http_response1_0_padded = http_response_plaintext_1.concat(Array(DATA_BYTES - http_response_plaintext_1.length).fill(0));
+        let http_response2_padded = http_response_plaintext_2.concat(Array(DATA_BYTES - http_response_plaintext_2.length).fill(-1));
+        let http_response2_0_padded = http_response_plaintext_2.concat(Array(DATA_BYTES - http_response_plaintext_2.length).fill(0));
+        let ciphertext1_padded = http_response_ciphertext_1.concat(Array(DATA_BYTES - http_response_ciphertext_1.length).fill(-1));
+        let ciphertext2_padded = http_response_ciphertext_2.concat(Array(DATA_BYTES - http_response_ciphertext_2.length).fill(-1));
 
-        const [ciphertext_digest, init_nivc_input] = InitialDigest(MockManifest(), [ciphertext_padded, ciphertext_padded_2], MAX_STACK_HEIGHT);
+        const [ciphertext_digest, init_nivc_input] = InitialDigest(MockManifest(), [ciphertext1_padded, ciphertext2_padded], MAX_STACK_HEIGHT);
 
-        assert.deepEqual(ciphertext_digest, check_ciphertext_digest_dup + check_ciphertext_digest);
+        let pt11Digest = PolynomialDigest(http_response1_0_padded, ciphertext_digest, BigInt(0));
+        let pt21Digest = PolynomialDigest(http_response2_0_padded, ciphertext_digest, BigInt(http_response_plaintext_1.length));
+        let ptDigest = PolynomialDigest(http_response_plaintext, ciphertext_digest, BigInt(0));
+        assert.deepEqual(pt11Digest + pt21Digest, ptDigest);
+
+        // assert.deepEqual(ciphertext_digest, check_ciphertext_digest_dup + check_ciphertext_digest);
         // assert.deepEqual(init_nivc_input, check_init_nivc_input_dup);
 
         const counterBits = uintArray32ToBits([1])[0];
@@ -179,34 +194,39 @@ describe("Example NIVC Proof", async () => {
         const nonceIn = toInput(Buffer.from([0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x4a, 0x00, 0x00, 0x00, 0x00]));
         let plaintext_authentication1 = await PlaintextAuthentication.compute({
             step_in: init_nivc_input,
-            plaintext: http_response_padded,
+            plaintext: http_response1_padded,
             key: keyIn,
             nonce: nonceIn,
             counter: counterBits,
+            plaintext_index_counter: 0,
+            ciphertext_digest,
         }, ["step_out"]);
 
-        let ct1Digest = DataHasher(ciphertext_padded);
-        assert.deepEqual(plaintext_authentication1.step_out, init_nivc_input - ct1Digest);
+        let pt1Digest = PolynomialDigest(http_response1_0_padded, ciphertext_digest, BigInt(0));
+        let ct1Digest = DataHasher(ciphertext1_padded);
+        assert.deepEqual(plaintext_authentication1.step_out, init_nivc_input - ct1Digest + pt1Digest);
 
         const nonceIn2 = toInput(Buffer.from([0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x4a, 0x00, 0x00, 0x00, 0x01]));
         let plaintext_authentication2 = await PlaintextAuthentication.compute({
             step_in: plaintext_authentication1.step_out,
-            plaintext: http_response_padded,
+            plaintext: http_response2_padded,
             key: keyIn,
             nonce: nonceIn2,
             counter: counterBits,
+            plaintext_index_counter: http_response_plaintext_1.length,
+            ciphertext_digest,
         }, ["step_out"]);
 
-        let ct2Digest = DataHasher(ciphertext_padded_2);
-        assert.deepEqual(plaintext_authentication2.step_out, init_nivc_input - ct1Digest - ct2Digest);
+        let pt2Digest = PolynomialDigest(http_response2_0_padded, ciphertext_digest, BigInt(http_response_plaintext_1.length));
+        let ct2Digest = DataHasher(ciphertext2_padded);
+        assert.deepEqual(plaintext_authentication2.step_out, init_nivc_input - ct1Digest - ct2Digest + pt1Digest + pt2Digest);
 
-        const http_response_plaintext_digest = PolynomialDigest(http_response_0_padded, ciphertext_digest);
-        const http_response_plaintext_digest_hashed = poseidon1([http_response_plaintext_digest]);
+        const http_response_plaintext_digest = PolynomialDigest(http_response_plaintext, ciphertext_digest, BigInt(0));
 
         // Run HTTPVerification
-        const start_line_digest = PolynomialDigest(http_start_line, ciphertext_digest);
-        const header_0_digest = PolynomialDigest(http_header_0, ciphertext_digest);
-        const header_1_digest = PolynomialDigest(http_header_1, ciphertext_digest);
+        const start_line_digest = PolynomialDigest(http_start_line, ciphertext_digest, BigInt(0));
+        const header_0_digest = PolynomialDigest(http_header_0, ciphertext_digest, BigInt(0));
+        const header_1_digest = PolynomialDigest(http_header_1, ciphertext_digest, BigInt(0));
 
         let main_digests = Array(MAX_NUMBER_OF_HEADERS + 1).fill(0);
         main_digests[0] = start_line_digest;
@@ -223,13 +243,13 @@ describe("Example NIVC Proof", async () => {
 
         const padded_http_body = http_body.concat(Array(DATA_BYTES - http_body.length).fill(0));
         let http_verification_step_out = BigInt((http_verification.step_out as number[])[0]);
-        let body_digest = PolynomialDigest(http_body, ciphertext_digest);
+        let body_digest = PolynomialDigest(http_body, ciphertext_digest, BigInt(0));
 
         const body_digest_hashed = poseidon1([body_digest]);
         const start_line_digest_digest_hashed = poseidon1([start_line_digest]);
         const header_0_digest_hashed = poseidon1([header_0_digest]);
         const header_1_digest_hashed = poseidon1([header_1_digest]);
-        const correct_http_verification_step_out = modAdd(step_in - start_line_digest_digest_hashed - header_0_digest_hashed - header_1_digest_hashed, body_digest_hashed);
+        const correct_http_verification_step_out = modAdd(step_in - http_response_plaintext_digest - start_line_digest_digest_hashed - header_0_digest_hashed - header_1_digest_hashed, body_digest_hashed);
         assert.deepEqual(http_verification_step_out, correct_http_verification_step_out);
 
         // Run JSONExtraction
@@ -248,7 +268,7 @@ describe("Example NIVC Proof", async () => {
 
         const [stack, treeHashes] = jsonTreeHasher(ciphertext_digest, keySequence, MAX_STACK_HEIGHT);
         const sequence_digest = compressTreeHash(ciphertext_digest, [stack, treeHashes]);
-        const value_digest = PolynomialDigest(targetValue, ciphertext_digest);
+        const value_digest = PolynomialDigest(targetValue, ciphertext_digest, BigInt(0));
 
         let json_extraction = await JSONExtraction.compute({
             step_in: http_verification_step_out,
