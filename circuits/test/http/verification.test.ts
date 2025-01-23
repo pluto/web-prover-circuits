eda7f9d0520741283e62b5528a10cb43c837464d
Colin Roberts <colin@autoparallel.xyz>
2025-01-23 15:16:21 -0700

    fix: `HTTPVerification` test -- no body

--- circuits/test/http/verification.test.ts
+++ circuits/test/http/verification.test.ts
@@ -1,4 +1,4 @@
-import { circomkit, WitnessTester, PolynomialDigest, http_response_plaintext, http_start_line, http_header_0, http_header_1, http_body, modAdd } from "../common";
+import { circomkit, WitnessTester, PolynomialDigest, http_response_plaintext, http_start_line, http_header_0, http_header_1, http_body, modAdd, PUBLIC_IO_VARIABLES } from "../common";
 import { assert } from "chai";
 import { poseidon1 } from "poseidon-lite";
 
@@ -24,33 +24,46 @@ const DATA_BYTES = 320;
 const MAX_NUMBER_OF_HEADERS = 2;
 
 describe("HTTP Verification", async () => {
-    let HTTPVerification: WitnessTester<["step_in", "data", "main_digests", "ciphertext_digest"], ["step_out"]>;
+    let HTTPVerification: WitnessTester<["step_in", "data", "machine_state", "main_digests", "ciphertext_digest"], ["step_out"]>;
     before(async () => {
         HTTPVerification = await circomkit.WitnessTester("http_nivc", {
             file: "http/verification",
             template: "HTTPVerification",
-            params: [DATA_BYTES, MAX_NUMBER_OF_HEADERS]
+            params: [DATA_BYTES, MAX_NUMBER_OF_HEADERS, PUBLIC_IO_VARIABLES]
         });
     });
     const mock_ct_digest = poseidon1([69]);
 
-    it("witness: http_response_plaintext, no header", async () => {
-        // Get all the hashes we need
-        let data_digest = PolynomialDigest(http_response_plaintext, mock_ct_digest, BigInt(0));
+    // Used across tests
+    let machine_state = Array(6).fill(0);
+    machine_state[0] = 1; // Sets the parsing start to 1
+    let step_in = Array(PUBLIC_IO_VARIABLES).fill(0);
+    step_in[3] = 1; // This would be the PD of the machine state given 1 is in the x^0 coeff
 
-        // Compute the HTTP info digest
-        let start_line_digest = PolynomialDigest(http_start_line, mock_ct_digest, BigInt(0));
-        let start_line_digest_hashed = poseidon1([start_line_digest]);
-        let body_digest = PolynomialDigest(http_body, mock_ct_digest, BigInt(0));
-        let body_digest_hashed = poseidon1([body_digest]);
-        // Use `modAdd` to get back a number between 0 and PRIME
-        let output_difference = modAdd(body_digest_hashed - start_line_digest_hashed - data_digest, BigInt(0));
+    // Get all the hashes we need
+    let plaintext_digest = PolynomialDigest(http_response_plaintext, mock_ct_digest, BigInt(0));
+
+    // Compute the HTTP info digest
+    let start_line_digest = PolynomialDigest(http_start_line, mock_ct_digest, BigInt(0));
+    let start_line_digest_hashed = poseidon1([start_line_digest]);
+    let header_0_digest = PolynomialDigest(http_header_0, mock_ct_digest, BigInt(0));
+    let header_0_digest_hashed = poseidon1([header_0_digest]);
+    let header_1_digest = PolynomialDigest(http_header_1, mock_ct_digest, BigInt(0));
+    let header_1_digest_hashed = poseidon1([header_1_digest]);
+    let body_digest = PolynomialDigest(http_body, mock_ct_digest, BigInt(0));
+    let output_difference = modAdd(-plaintext_digest, BigInt(0)); // TODO: need to add body_digest
+
+    it("witness: http_response_plaintext, no header", async () => {
+        // For this specific test, we need these registers set
+        step_in[4] = start_line_digest_hashed;
+        step_in[5] = 1; // Total number of matches to expect (sl)
 
         // Run the HTTP circuit
         // POTENTIAL BUG: I didn't get this to work with `expectPass` as it didn't compute `step_out` that way???
         let http_nivc_compute = await HTTPVerification.compute({
-            step_in: 0,  // This doesn't really matter for this test
+            step_in,  // This doesn't really matter for this test
             data: http_response_plaintext,
+            machine_state,
             main_digests: [start_line_digest].concat(Array(2).fill(0)),
             ciphertext_digest: mock_ct_digest
         }, ["step_out"]);
@@ -59,24 +72,16 @@ describe("HTTP Verification", async () => {
     });
 
     it("witness: http_response_plaintext, one header", async () => {
-        // Get all the hashes we need
-        let data_digest = PolynomialDigest(http_response_plaintext, mock_ct_digest, BigInt(0));
-
-        // Compute the HTTP info digest
-        let start_line_digest = PolynomialDigest(http_start_line, mock_ct_digest, BigInt(0));
-        let start_line_digest_hashed = poseidon1([start_line_digest]);
-        let header_0_digest = PolynomialDigest(http_header_0, mock_ct_digest, BigInt(0));
-        let header_0_digest_hashed = poseidon1([header_0_digest]);
-        let body_digest = PolynomialDigest(http_body, mock_ct_digest, BigInt(0));
-        let body_digest_hashed = poseidon1([body_digest]);
-        // Use `modAdd` to get back a number between 0 and PRIME
-        let output_difference = modAdd(body_digest_hashed - start_line_digest_hashed - header_0_digest_hashed - data_digest, BigInt(0));
+        // For this specific test, we need these registers set
+        step_in[4] = start_line_digest_hashed + header_0_digest_hashed;
+        step_in[5] = 2; // Total number of matches to expect (sl, h0)
 
         // Run the HTTP circuit
         // POTENTIAL BUG: I didn't get this to work with `expectPass` as it didn't compute `step_out` that way???
         let http_nivc_compute = await HTTPVerification.compute({
-            step_in: 0,  // This doesn't really matter for this test
+            step_in,  // This doesn't really matter for this test
             data: http_response_plaintext,
+            machine_state,
             main_digests: [start_line_digest, header_0_digest].concat(Array(1).fill(0)),
             ciphertext_digest: mock_ct_digest
         }, ["step_out"]);
@@ -85,26 +90,16 @@ describe("HTTP Verification", async () => {
     });
 
     it("witness: http_response_plaintext, two headers", async () => {
-        // Get all the hashes we need
-        let data_digest = PolynomialDigest(http_response_plaintext, mock_ct_digest, BigInt(0));
-
-        // Compute the HTTP info digest
-        let start_line_digest = PolynomialDigest(http_start_line, mock_ct_digest, BigInt(0));
-        let start_line_digest_hashed = poseidon1([start_line_digest]);
-        let header_0_digest = PolynomialDigest(http_header_0, mock_ct_digest, BigInt(0));
-        let header_0_digest_hashed = poseidon1([header_0_digest]);
-        let header_1_digest = PolynomialDigest(http_header_1, mock_ct_digest, BigInt(0));
-        let header_1_digest_hashed = poseidon1([header_1_digest]);
-        let body_digest = PolynomialDigest(http_body, mock_ct_digest, BigInt(0));
-        let body_digest_hashed = poseidon1([body_digest]);
-        // Use `modAdd` to get back a number between 0 and PRIME
-        let output_difference = modAdd(body_digest_hashed - start_line_digest_hashed - header_0_digest_hashed - header_1_digest_hashed - data_digest, BigInt(0));
+        // For this specific test, we need these registers set
+        step_in[4] = start_line_digest_hashed + header_0_digest_hashed + header_1_digest_hashed;
+        step_in[5] = 3; // Total number of matches to expect (sl, h0, h1)
 
         // Run the HTTP circuit
         // POTENTIAL BUG: I didn't get this to work with `expectPass` as it didn't compute `step_out` that way???
         let http_nivc_compute = await HTTPVerification.compute({
-            step_in: 0,  // This doesn't really matter for this test
+            step_in,  // This doesn't really matter for this test
             data: http_response_plaintext,
+            machine_state,
             main_digests: [start_line_digest, header_0_digest, header_1_digest],
             ciphertext_digest: mock_ct_digest
         }, ["step_out"]);
@@ -113,30 +108,21 @@ describe("HTTP Verification", async () => {
     });
 
     it("witness: http_response_plaintext, two headers, order does not matter", async () => {
-        // Get all the hashes we need
-        let data_digest = PolynomialDigest(http_response_plaintext, mock_ct_digest, BigInt(0));
-
-        // Compute the HTTP info digest
-        let start_line_digest = PolynomialDigest(http_start_line, mock_ct_digest, BigInt(0));
-        let start_line_digest_hashed = poseidon1([start_line_digest]);
-        let header_0_digest = PolynomialDigest(http_header_0, mock_ct_digest, BigInt(0));
-        let header_0_digest_hashed = poseidon1([header_0_digest]);
-        let header_1_digest = PolynomialDigest(http_header_1, mock_ct_digest, BigInt(0));
-        let header_1_digest_hashed = poseidon1([header_1_digest]);
-        let body_digest = PolynomialDigest(http_body, mock_ct_digest, BigInt(0));
-        let body_digest_hashed = poseidon1([body_digest]);
-        // Use `modAdd` to get back a number between 0 and PRIME
-        let output_difference = modAdd(body_digest_hashed - start_line_digest_hashed - header_0_digest_hashed - header_1_digest_hashed - data_digest, BigInt(0));
+        // For this specific test, we need these registers set
+        step_in[4] = start_line_digest_hashed + header_0_digest_hashed + header_1_digest_hashed;
+        step_in[5] = 3; // Total number of matches to expect (sl, h0, h1)
 
         // Run the HTTP circuit
         // POTENTIAL BUG: I didn't get this to work with `expectPass` as it didn't compute `step_out` that way???
         let http_nivc_compute = await HTTPVerification.compute({
-            step_in: 0,  // This doesn't really matter for this test
+            step_in,  // This doesn't really matter for this test
             data: http_response_plaintext,
+            machine_state,
             main_digests: [header_1_digest, start_line_digest, header_0_digest],
             ciphertext_digest: mock_ct_digest
         }, ["step_out"]);
         // I fucking hate circomkit
+        // TODO: need to check more of the assertions
         assert.deepEqual((http_nivc_compute.step_out as BigInt[])[0], output_difference);
     });
 });
